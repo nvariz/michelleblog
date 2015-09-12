faf59dd4303d9f165a687b221de12e4a43fc4aba
Nicholas Variz <nicholasvariz@gmail.com>
2015-09-12 11:06:06 -0700

    Use haml

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-== README
+# README
 
 ## System Dependencies
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index' 
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :posts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
