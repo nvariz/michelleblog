6e45ba1fe961c07c6cf7b7f70a4c67492533247e
Nicholas Variz <nicholasvariz@gmail.com>
2015-09-12 12:16:34 -0700

    basic post crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,35 @@
 class PostsController < ApplicationController
+  before_action :post, only: [:edit, :show]
+
+  def new
+    @post = Post.new
+
+    render 'form'
+  end
+
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = Post.new(post_params)
 
     @post.save ? redirect_to(@post) : render('new')
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def edit
+    render 'form'
+  end
+
+  def update
+    post.update!(post_params)
+
+    redirect_to post
+  end
+
+  private
+
+  def post
+    @post ||= Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
