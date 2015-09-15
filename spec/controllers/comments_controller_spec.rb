d4c7bfec689b934f01a082c1c51a33e2c83b6004
Nicholas Variz <nicholasvariz@gmail.com>
2015-09-14 19:37:01 -0700

    WIP Comments ctrlr spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,38 @@
-require 'rails_helper'
+describe CommentsController do
+  describe '#create' do
+    let!(:_post)   { Post.create(title: 'title', body: 'body') }
+    let(:subject) { post :create, post_id: _post.id, comment: params }
 
-RSpec.describe CommentsController, type: :controller do
+    context 'with invalid params' do
+      let(:params) { { name: '', body: 'bar' } }
 
+      it 'does not redirect to show page' do
+        expect(subject).to render_template('posts/show')
+      end
+
+      it 'renders error flash message' do
+        post :create, post_id: _post.id, comment: params
+
+        expect(flash[:notice]).to eq("Name can't be blank")
+      end
+    end
+
+    context 'with valid params' do
+      let(:params) { { name: 'foo', body: 'bar' } }
+
+      it 'redirects to show page' do
+        expect(subject).to redirect_to(post_path(_post))
+      end
+
+      it 'renders a success flash message' do
+        post :create, post_id: _post.id, comment: params
+
+        expect(flash[:notice]).to eq('Comment saved successfully')
+      end
+
+      it 'creates a new comment record' do
+        expect { subject }.to change { Comment.count }.by(1)
+      end
+    end
+  end
 end
