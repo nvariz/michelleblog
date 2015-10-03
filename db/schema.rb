52bc260ecf8c1cb70a7e67ed43e64067c2de5a8e
Nicholas Variz <nicholasvariz@gmail.com>
2015-10-03 09:24:19 -0700

    Remove unnecessary devise views

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,11 @@ ActiveRecord::Schema.define(version: 20150926010532) do
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
